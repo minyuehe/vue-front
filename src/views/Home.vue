33499e826f9e9b470d121a474f5c55425347d10e
minyuecmhe <minyuecmhe@tencent.com>
2021-10-29 15:05:39 +0800

    fix: 初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your home"/>
+    <LoginForm />
+    <el-button>默认按钮</el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    LoginForm
   }
 }
 </script>
